2ccc06c575bdecade02e075b65b3b46f95cd9fd6
Majharul tanvir <mdtanvir6070@gmail.com>
2022-03-11 12:09:22 +0600

    5 & 6 ans done

--- js/app.js
+++ js/app.js
@@ -12,11 +12,11 @@ const getReportedPosts = () => {
 };
 
 const isLiked = (id) => {
-    return likedPostsId ? likedPostsId.length : !!likedPostsId.includes(id);
+    return likedPostsId ? .length && !!likedPostsId.includes(id);
 };
 
 const addToLiked = (id) => {
-    likedPostsId.plus(id);
+    likedPostsId.push(id);
     showPosts(posts);
 };
 
@@ -152,7 +152,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById("reported").appendChild(div);
     });
